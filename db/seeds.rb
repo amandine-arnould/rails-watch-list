a3a055a208dc0a4aa7d0b8d5eb356c69514d499f
amandine arnould <amandine.arnould90@gmail.com>
2021-08-12 17:56:34 +0200

    App functionnal - without options

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.destroy_all
+List.destroy_all
+
+Movie.create(
+  title: "Wonder Woman 1984",
+  overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
+  poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
+  rating: 6.9
+)
+Movie.create(
+  title: "The Shawshank Redemption",
+  overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison",
+  poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+  rating: 8.7
+)
+Movie.create(
+  title: "Titanic",
+  overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.",
+  poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
+  rating: 7.9
+)
+Movie.create(
+  title: "Ocean's Eight",
+  overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.",
+  poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
+  rating: 7.0
+)
+
+
+List.create(
+  name: "Clasic movies",
+  image_url: "https://cdn.dribbble.com/users/2598141/screenshots/11444902/media/d3556b22f54a66ddc080dd90bbd2dd04.jpg?compress=1&resize=1600x1200"
+)
+
+List.create(
+  name: "Superhero",
+  image_url: "https://cdn.dribbble.com/users/4327707/screenshots/9391828/media/81a2461f8d6f977c5a2838c739d0bd0d.jpg?compress=1&resize=800x600"
+)
